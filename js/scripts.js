aba4920a6f9835fb1e31981dc81870d18fde8204
Mupeshpensia <kumarpensia@gmail.com>
2021-05-20 10:37:28 +0530

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+
+(function($) {
+    "use strict"; 
+	
+	/* Preloader */
+	$(window).on('load', function() {
+		var preloaderFadeOutTime = 500;
+		function hidePreloader() {
+			var preloader = $('.spinner-wrapper');
+			setTimeout(function() {
+				preloader.fadeOut(preloaderFadeOutTime);
+			}, 500);
+		}
+		hidePreloader();
+	});
+
+	
+	/* Navbar Scripts */
+	// jQuery to collapse the navbar on scroll
+    $(window).on('scroll load', function() {
+		if ($(".navbar").offset().top > 60) {
+			$(".fixed-top").addClass("top-nav-collapse");
+		} else {
+			$(".fixed-top").removeClass("top-nav-collapse");
+		}
+    });
+
+
+
+
+})(jQuery);
